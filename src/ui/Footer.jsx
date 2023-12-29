50112f49cdc34fe1967e6fa8523243db6b70bcaf
Adamskid9 <ajaniadam9@gmail.com>
2023-12-29 23:29:43 +0100

    space

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -2,7 +2,7 @@ function Footer({styles,parent}) {
     
     return (
         <div className={`${parent} p-2  sm:p-5 bg-blue-900`}>
-          <div className={`${styles} font-[600] tracking-[.3px] text-sm sm:text-base font-Bellefair `}>
+          <div className={`${styles} font-[200] tracking-[.3px] text-sm sm:text-base font-Bellefair `}>
             Coded by Adam, Challenge by Frontend Mentor
           </div>
         </div>
