814be20e192059f047b6e7b882727dc415fa3e01
Adamskid9 <ajaniadam9@gmail.com>
2023-12-29 23:28:03 +0100

    space

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer({styles,parent}) {
     
     return (
-        <div className={`${parent} py-2 sm:p-5 bg-blue-900`}>
-          <div className={`${styles} text-black tracking-[1px] font-[600] text-sm sm:text-base font-Bellefair `}>
-            Coded by<span className="mx-1 text-white">Adam</span>,Challenge by<span className="ml-1 text-white">Frontend Mentor</span>
+        <div className={`${parent} p-2  sm:p-5 bg-blue-900`}>
+          <div className={`${styles} font-[600] tracking-[.3px] text-sm sm:text-base font-Bellefair `}>
+            Coded by Adam, Challenge by Frontend Mentor
           </div>
         </div>
     )
